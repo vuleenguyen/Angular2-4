8109fed79f26cec90438c3fd3844ea3da2201be3
vuleenguyen <vuleenguyen.92@gmail.com>
2017-08-04 14:52:50 -0700

    update angular JS doc

--- ng4-complete-guide/src/app/app-routing.module.ts
+++ ng4-complete-guide/src/app/app-routing.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-
-
 const appRoutes: Routes = [
     {
-        path: '/recipes', component: RecipesComponent
+        path: '', redirectTo: '/recipes', pathMatch: 'full'
     },
     {
-        path: '/shopping-list', component: ShoppingListComponent
+        path: 'recipes', component: RecipesComponent
+    },
+    {   
+        path: 'shopping-list', component: ShoppingListComponent
     }
 ]
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(appRoutes)
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports : [RouterModule]
 })
 export class AppRoutingModule {
 
